2020c2517a5c402294141b088317d26cea4576c9
unknown <allmeida.lima@gmail.com>
2022-08-07 18:48:52 -0300

    modificações

--- relógio_tkinter.py
+++ relógio_tkinter.py
@@ -1,45 +1,95 @@
+"""
+root = Tk()
+root.title("Relógio Mundial")
+ttk.Label(
+    root,
+    font=('Comicsans, 30')
+        )
+    hour.after(1000, time_country)
+       
+root.mainloop()
+
+time_country()
+#root.after(1000, time_country)
+
+
+
+clock = Label()
+clock.pack()
+clock['font'] = 'Roboto 100 bold'
+clock['text'] = strftime('%H:%M:%S')
+
+def get_current_time():
+    now = strftime('%H:%M:%S')
+    if now != clock['text']:
+        clock['text'] = now
+    clock.after(100, get_current_time)
+
+get_current_time()
+clock.mainloop()
+"""
+
 from tkinter import *
 from tkinter import ttk
 import datetime
+from ttkbootstrap import Style
+from time import strftime
 import pytz
 
-class Clock:
-    def __init__(self,master=None):
-        self.widget = master.title("Clock")
-        self.widget = Frame(master)
-        self.hour = ttk.Label(
-            self.widget,
-            font=('Comicsans, 30')
+style = Style()
+root = style.master
 
-        )
-        self.hour.pack()
-        self.time_country()
-        self.hour.grid(row=0, column=1, sticky=W, pady=2)
-        self.hour.pack()
-        
-        self.phrase = ttk.Label(
-            text="Sigla do Pais"
-        )
-        self.phrase.pack(side=LEFT)
-        #self.options = StringVar()
-        #self.combo = ttk.Combobox(self.widget, textvariable=self.options)
-        #self.combo['values'] = [co for co in pytz.country_timezones]
-        #self.combo['state'] = 'readonly'
-        #self.combo['width'] = 5
-        #self.combo.pack(side=LEFT, padx=10, pady=20)
+root.title('Relógio Mundial')
 
-    def time_country(self):
-        country_huor = datetime.datetime.now()
-        self.hour['text'] = country_huor.strftime('%H:%M:%S')
-        self.widget.after(1000, self.time_country)
-        
+frame_geral = ttk.Frame()
 
+def time_country():
+    country_huor = datetime.datetime.now()
+    hour = country_huor.strftime('%H:%M:%S')
+    print(hour)
 
+values_countrys = [tz for tz in pytz.country_timezones]
+#print(values)
 
+#Sigla paises
+frame_sigla_pais = ttk.Frame(frame_geral)
 
+label_sigla_pais = ttk.Label(
+    frame_sigla_pais,
+    text='Pais',
+    font=(None, 20)
+)
+combo_countrys = ttk.Combobox(
+    frame_sigla_pais,
+    values=values_countrys,
+    font=(None, 20)
+)
+combo_countrys.set("BR")
+resultado = combo_countrys.get()
 
+label_sigla_pais.grid(row=0, column=0, padx=10, pady=10)
+combo_countrys.grid(row=0, column=1, padx=10, pady=10)
+frame_sigla_pais.pack()
 
+values_estados = [es for es in pytz.country_timezones(resultado)]
+print(values_estados)
+#Estados
+frame_estados = ttk.Frame(frame_geral)
 
-root = Tk()
-Clock(root)
+label_estados = ttk.Label(
+    frame_estados,
+    text='Estado',
+    font=(None, 20)
+)
+combo_estados = ttk.Combobox(
+    frame_estados,
+    values=values_estados,
+    font=(None, 20)
+)
+
+label_estados.grid(row=0, column=0, padx=10, pady=10)
+combo_estados.grid(row=0, column=1, padx=10, pady=10)
+frame_estados.pack()
+
+frame_geral.pack()
 root.mainloop()
